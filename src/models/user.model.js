132acefcd8d8057c592a336db912c545c92c98a5
ritammaity55 <ritammaity55@gmail.com>
2024-10-10 00:47:40 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,8 @@
-import mongoose from "mongoose"
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const userSchema = new UserSchema(
+const userSchema = new Schema(
   {
     username: {
       type: String,
@@ -47,17 +47,18 @@ const userSchema = new UserSchema(
     },
   },
   { timestamps: true }
-)
+);
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next()
-  this.password = bcrypt.hash(this.password, 10)
-  next()
-})
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password)
-}
+  return await bcrypt.compare(password, this.password);
+};
 
 userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
@@ -71,8 +72,8 @@ userSchema.methods.generateAccessToken = function () {
     {
       expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-  )
-}
+  );
+};
 userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
@@ -82,7 +83,7 @@ userSchema.methods.generateRefreshToken = function () {
     {
       expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
-  )
-}
+  );
+};
 
-export const User = mongoose.model("User", userSchema)
+export const User = mongoose.model("User", userSchema);
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,23 +1,23 @@
-class ApiError {
+class ApiError extends Error {
   constructor(
     statusCode,
-    message = "Something went wrong ",
+    message = "Something went wrong",
     errors = [],
     stack = ""
   ) {
-    super(message)
-    this.statusCode = statusCode
-    this.data = null
-    this.message = message
-    this.success = false
-    this.errors = this.errors
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
 
     if (stack) {
-      this.stack = stack
+      this.stack = stack;
     } else {
-      Error.captureStackTrace(this, this.constructor)
+      Error.captureStackTrace(this, this.constructor);
     }
   }
 }
 
-export { ApiError }
+export { ApiError };
